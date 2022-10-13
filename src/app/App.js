7d327debca4d5df34c9fab14882363e9f5cfaf17
deniso4ka <den.zagrosh@gmail.com>
2022-10-13 16:53:57 +0300

    feat: locale

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import cls from 'classnames'
 import './styles/index.css'
+import { useTranslation } from 'react-i18next';
 
 function App() {
   const [theme, setTheme] = React.useState('default')
   const toggleTheme = () => {
     setTheme((prev) => prev === 'default' ? 'dark' : 'default')
   }
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
+
+  const { t, i18n } = useTranslation();
+
   return (
     <div className={cls('app', theme === 'default' ? '' : 'dark')}>
-      junion-backoffice
+      {t('main')}
+      <button onClick={toggleLang}>{t('button')}</button>
       <button onClick={toggleTheme}>toggle</button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 
+import './app/config/i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Suspense fallback="loading">
+    <App />
+  </Suspense>
 );
