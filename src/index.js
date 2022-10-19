2b87e03eb42f03716c013297a43d9aa6009b9c09
deniso4ka <den.zagrosh@gmail.com>
2022-10-19 03:33:50 +0300

    feat: store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 
 import './shared/config/i18n'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Suspense fallback='loading'>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </Suspense>
 )
