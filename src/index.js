d76371c7951c73453e13cb1bf04c6e6b6aef4bd3
deniso4ka <den.zagrosh@gmail.com>
2022-10-19 03:28:33 +0300

    refactor: structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App'
 
-import './app/config/i18n';
+import './shared/config/i18n'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Suspense fallback='loading'>
         <App />
-    </Suspense>,
-);
+    </Suspense>
+)
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import cls from 'classnames';
-import s from './Login.module.css';
+import React from 'react'
+import cls from 'classnames'
+import s from './Login.module.css'
 
 function Login() {
-    return <div className={s.wrapper}>Login</div>;
+    return (
+        <main className={s.wrapper}>
+            <div className={s.login_block}>
+                <div className={s.title} />
+                <input type='text' className={s.login} />
+                <input type='text' className={s.password} />
+            </div>
+        </main>
+    )
 }
 
-export default Login;
+export default Login
