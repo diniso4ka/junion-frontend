957c37e5272f2653ea4e9b785993e0ab94feb796
deniso4ka <den.zagrosh@gmail.com>
2022-11-15 23:40:12 +0300

    refactor: sidebar

--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -15,6 +15,13 @@ import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false)
 
+    const navLinks = [
+        { label: 'Home', icon: home, path: routeConfig.HOME },
+        { label: 'Categories', icon: categories, path: routeConfig.CATEGORIES },
+        { label: 'Products', icon: products, path: routeConfig.PRODUCTS },
+        { label: 'Vendors', icon: vendors, path: routeConfig.VENDORS },
+    ]
+
     return (
         <aside className={cls(s.sidebar, { [s.collapsed]: collapsed })}>
             <nav className={s.menu}>
@@ -30,61 +37,16 @@ const Sidebar = () => {
                             <img className={s.image} src={burger} />
                         </Button>
                     </li>
-                    {/*<li className={s.item}>*/}
-                    {/*    <Link className={cls(s.link)} to={routeConfig.HOME}>*/}
-                    {/*        <div className={s.linkContent}>*/}
-                    {/*            <img className={s.image} src={home} />*/}
-                    {/*            <p className={s.text}>Home</p>*/}
-                    {/*        </div>*/}
-                    {/*    </Link>*/}
-                    {/*</li>*/}
-                    {/*<li className={s.item}>*/}
-                    {/*    <Link className={s.link} to={routeConfig.CATEGORIES}>*/}
-                    {/*        <img className={s.image} src={categories} />*/}
-                    {/*    </Link>*/}
-                    {/*</li>*/}
-                    {/*<li className={s.item}>*/}
-                    {/*    <Link className={s.link} to={routeConfig.PRODUCTS}>*/}
-                    {/*        <img className={s.image} src={products} />*/}
-                    {/*    </Link>*/}
-                    {/*</li>*/}
-                    {/*<li className={s.item}>*/}
-                    {/*    <Link className={s.link} to={routeConfig.VENDORS}>*/}
-                    {/*        <img className={s.image} src={vendors} />*/}
-                    {/*    </Link>*/}
-                    {/*</li>*/}
-                    <Link
-                        variant={'navigation'}
-                        icon={home}
-                        to={routeConfig.HOME}
-                        isCollapsed={!collapsed}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        variant={'navigation'}
-                        icon={categories}
-                        to={routeConfig.CATEGORIES}
-                        isCollapsed={!collapsed}
-                    >
-                        Categories
-                    </Link>
-                    <Link
-                        variant={'navigation'}
-                        icon={products}
-                        to={routeConfig.PRODUCTS}
-                        isCollapsed={!collapsed}
-                    >
-                        Products
-                    </Link>
-                    <Link
-                        variant={'navigation'}
-                        icon={vendors}
-                        to={routeConfig.VENDORS}
-                        isCollapsed={!collapsed}
-                    >
-                        Vendors
-                    </Link>
+                    {navLinks.map(element => (
+                        <Link
+                            isCollapsed={!collapsed}
+                            icon={element.icon}
+                            variant={'navigation'}
+                            to={element.path}
+                        >
+                            {element.label}
+                        </Link>
+                    ))}
                 </ul>
             </nav>
         </aside>
