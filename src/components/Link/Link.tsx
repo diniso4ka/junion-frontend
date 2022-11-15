244f97ebbc1042d6c066b8cc7f3d3b6ff54dc313
deniso4ka <den.zagrosh@gmail.com>
2022-11-15 23:13:29 +0300

    feat: link and sidebar

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -6,19 +6,34 @@ import s from './Link.module.scss'
 
 interface ILinkProps extends LinkProps {
     children: React.ReactNode
-    variant?: 'primary' | 'secondary' | 'outline' | 'clear'
+    variant?: 'primary' | 'secondary' | 'outline' | 'clear' | 'navigation'
     className?: string
+    icon?: string
+    isCollapsed?: boolean
 }
 
 export const Link: React.FC<ILinkProps> = React.memo(
-    ({ children, variant = 'primary', className, ...rest }) => {
+    ({
+        children,
+        variant = 'primary',
+        className,
+        icon,
+        isCollapsed = false,
+        ...rest
+    }) => {
         const match = useMatch(rest.to as string)
         const classes = cls(s.link, s[variant], className, {
             [s.active]: match,
+            [s.collapsed]: !isCollapsed,
         })
         return (
             <div className={classes}>
-                <LinkButton {...rest}>{children}</LinkButton>
+                <LinkButton className={s.linkWrapper} {...rest}>
+                    <div className={s.iconContainer}>
+                        {icon && <img className={s.icon} src={icon} />}
+                    </div>
+                    <span className={s.label}>{children}</span>
+                </LinkButton>
             </div>
         )
     }
