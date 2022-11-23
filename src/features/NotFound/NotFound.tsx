466318c5779050b21412577a2668ffae2d15bbea
Evgenii <evgenypetrov5230@gmail.com>
2022-11-23 16:18:28 +0300

    feat: not-found

---
+++ src/features/NotFound/NotFound.tsx
@@ -0,0 +1,24 @@
+//@ts-nocheck
+import { useNavigate } from 'react-router'
+import { Button } from 'components'
+import s from './NotFound.module.scss'
+
+const NotFound = () => {
+    const navigate = useNavigate()
+    const goBackHandler = () => {
+        navigate('..', { relative: 'path' })
+    }
+
+    return (
+        <div className={s.notFoundContainer}>
+            <p className={s.statusCode}>404</p>
+            <p className={s.statusText}>Not found!</p>
+            <p className={s.statusDescription}>
+                The requested resource was not found on this server!
+            </p>
+            <Button onClick={goBackHandler}>Go back</Button>
+        </div>
+    )
+}
+
+export default NotFound
