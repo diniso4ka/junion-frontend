56b222d9b5b61abdb350e0bd3f4e084cbef3d9c3
deniso4ka <den.zagrosh@gmail.com>
2022-11-25 18:08:42 +0300

    refactor: filter

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,13 +3,16 @@ import s from './Search.module.scss'
 import cls from 'classnames'
 
 import searchIcon from 'shared/assets/images/icons/search.svg'
+import deleteIcon from 'shared/assets/images/icons/delete.svg'
 import moreIcon from 'shared/assets/images/icons/more.svg'
 import { DroppedMenu } from 'components'
 import { FilterMenu } from 'pages/Private/Products/FilterMenu/FilterMenu'
 
 interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {
     className?: string
-    onFilterOpen: () => void
+    onFilterOpen?: () => void
+    onClear?: () => void
+    canClear?: boolean
     isOpened: boolean
     onClick: (e) => void
 }
@@ -18,6 +21,8 @@ export const Search: FC<SearchProps> = ({
     className,
     onFilterOpen,
     isOpened,
+    onClear,
+    canClear,
     onClick,
     ...rest
 }) => {
@@ -35,12 +40,19 @@ export const Search: FC<SearchProps> = ({
                 src={moreIcon}
                 onClick={onFilterOpen}
             />
+            {canClear && (
+                <img
+                    className={cls(s.helperIcon, s.helperSecondIconLeft)}
+                    src={deleteIcon}
+                    onClick={onClear}
+                />
+            )}
             <DroppedMenu
                 className={s.droppedMenu}
                 size={'large'}
                 isOpened={isOpened}
             >
-                <FilterMenu setIsOpen={onFilterOpen} />
+                <FilterMenu onClose={onFilterOpen} onClear={onClear} />
             </DroppedMenu>
         </div>
     )
--- src/pages/Private/Products/FilterMenu/FilterMenu.tsx
+++ src/pages/Private/Products/FilterMenu/FilterMenu.tsx
@@ -4,7 +4,6 @@ import cls from 'classnames'
 
 import { useAppDispatch, useAppSelector } from 'app/store/types'
 import { useSearchParams } from 'react-router-dom'
-import { createQueryParams } from 'shared/helpers/filters/createQueryParams'
 import {
     thunkFetchFiltredProductList,
     thunkFetchProductList,
@@ -15,24 +14,33 @@ import { Button, Input } from 'components'
 import { clearFiltredItems } from 'app/store/slices/products/productsSlice'
 import { ProductsFilterDefault } from 'shared/helpers/degaultValues/filters'
 import { InputWithHint } from 'components/InputWithHint'
+import {
+    setFilters,
+    setQueryString,
+} from 'app/store/slices/productsFilters/productsFilters'
 
 interface ProfileMenuProps {
-    setIsOpen?: (active) => void
+    onClose?: () => void
+    onClear?: () => void
     className?: string
     onClick?: () => void
 }
 
-export const FilterMenu: FC<ProfileMenuProps> = ({ className, setIsOpen }) => {
+export const FilterMenu: FC<ProfileMenuProps> = ({
+    className,
+    onClose,
+    onClear,
+}) => {
     const { categories, items } = useAppSelector(state => state.products.data)
     const dispatch = useAppDispatch()
+    const { filters, queryString } = useAppSelector(
+        state => state.productsFilters
+    )
     const [categoriesFocus, setCategoriesFocus] = useState(false)
     const [productsFocus, setProductsFocus] = useState(false)
 
     const [searchParams, setSearchParams] = useSearchParams()
-    const [filtersValue, setFiltersValue] = useState<IProductsFilter>(
-        ProductsFilterDefault
-    )
-    const [filters, setFilters] = useState({})
+    const [filtersValue, setFiltersValue] = useState<IProductsFilter>({})
 
     const onHandleProductHint = hint => {
         setFiltersValue(prev => ({ ...prev, name: hint }))
@@ -42,73 +50,107 @@ export const FilterMenu: FC<ProfileMenuProps> = ({ className, setIsOpen }) => {
     }
 
     const onSubmitFilters = () => {
-        setParamFilters(filtersValue)
-        const params = createQueryParams(filtersValue)
-        if (params) {
-            dispatch(thunkFetchFiltredProductList(params))
-        } else {
-            dispatch(thunkFetchProductList())
+        if (filtersValue) {
+            dispatch(setFilters(filtersValue))
         }
-        setIsOpen(false)
-    }
-
-    const setParamFilters = params => {
-        let filters: any = {}
-        for (const i in params) {
-            if (params[i].length > 0) {
-                filters = { [i]: params[i] }
-            }
-        }
-        setFilters(() => ({ ...filters }))
-        setFiltersValue(() => ({ ...filters }))
-    }
-
-    const onClearFilters = () => {
-        setSearchParams({})
-        setFiltersValue(ProductsFilterDefault)
-        setIsOpen(false)
+        onClose()
     }
 
     useEffect(() => {
-        const params: IProductsFilter = {}
-        const name = searchParams.get('name')
-        if (name) {
-            params.name = name
+        if (!!Object.values(filters).length || !!queryString) {
+            console.log(Object.values(filters).length || queryString)
+            dispatch(thunkFetchFiltredProductList(queryString))
+            setSearchParams(queryString)
+        } else {
+            console.log('false')
+            setFiltersValue({})
         }
-        const category = searchParams.get('category')
-        if (category) {
-            params.category = category
-        }
-        const vendor = searchParams.get('vendor')
-        if (vendor) {
-            params.vendor = vendor
-        }
-        const owner = searchParams.get('owner')
-        if (owner) {
-            params.owner = owner
-        }
-        const price_min = searchParams.get('price_min')
-        if (price_min) {
-            params.price_min = price_min
-        }
-        const price_max = searchParams.get('price_max')
-        if (price_max) {
-            params.price_max = price_max
-        }
-        if (Object.keys(params).length > 0) {
-            setParamFilters(params)
+    }, [filters, queryString])
+
+    useEffect(() => {
+        // @ts-ignore
+        const params = [...searchParams]
+        const objParams = {}
+        if (params.length > 0) {
+            console.log(params)
+            Object.values(params).forEach(
+                item => (objParams[item[0]] = item[1])
+            )
+            dispatch(setFilters(objParams))
+            setFiltersValue(objParams)
         }
     }, [])
 
-    useEffect(() => {
-        if (Object.keys(filters).length > 0) {
-            setFiltersValue(() => ({ ...filters }))
-            setSearchParams({ ...filters })
-            dispatch(thunkFetchFiltredProductList(createQueryParams(filters)))
-        } else {
-            dispatch(clearFiltredItems())
-        }
-    }, [filters])
+    //
+    // const onSubmitFilters = () => {
+    //     setParamFilters(filtersValue)
+    //     // if (filters.queryString) {
+    //     //     dispatch(thunkFetchFiltredProductList(filters.queryString))
+    //     // } else {
+    //     //     dispatch(thunkFetchProductList())
+    //     // }
+    //     setIsOpen(false)
+    // }
+    //
+    // const setParamFilters = params => {
+    //     let filters: any = {}
+    //     for (const i in params) {
+    //         if (params[i].length > 0) {
+    //             filters = { [i]: params[i] }
+    //         }
+    //     }
+    //     dispatch(setFilter(filtersValue))
+    //     setFiltersValue(() => ({ ...filters }))
+    // }
+    //
+    // const onClearFilters = () => {
+    //     setSearchParams({})
+    //     setFiltersValue(ProductsFilterDefault)
+    //     setIsOpen(false)
+    // }
+    //
+    // useEffect(() => {
+    //     const params: IProductsFilter = {}
+    //     const name = searchParams.get('name')
+    //     if (name) {
+    //         params.name = name
+    //     }
+    //     const category = searchParams.get('category')
+    //     if (category) {
+    //         params.category = category
+    //     }
+    //     const vendor = searchParams.get('vendor')
+    //     if (vendor) {
+    //         params.vendor = vendor
+    //     }
+    //     const owner = searchParams.get('owner')
+    //     if (owner) {
+    //         params.owner = owner
+    //     }
+    //     const price_min = searchParams.get('price_min')
+    //     if (price_min) {
+    //         params.price_min = price_min
+    //     }
+    //     const price_max = searchParams.get('price_max')
+    //     if (price_max) {
+    //         params.price_max = price_max
+    //     }
+    //     if (Object.keys(params).length > 0) {
+    //         setParamFilters(params)
+    //     }
+    // }, [])
+    //
+    // useEffect(() => {
+    //     if (Object.keys(filters).length > 0) {
+    //         setFiltersValue(() => ({ ...filters.filters }))
+    //         setSearchParams({ ...filters.filters })
+    //         dispatch(
+    //             thunkFetchFiltredProductList(createQueryParams(filters.filters))
+    //         )
+    //     } else {
+    //         dispatch(clearFiltredItems())
+    //     }
+    // }, [filters.filters])
 
     return (
         <div className={cls(s.FilterMenu, className)}>
@@ -212,13 +254,6 @@ export const FilterMenu: FC<ProfileMenuProps> = ({ className, setIsOpen }) => {
                     onClick={onSubmitFilters}
                 >
                     send
-                </Button>
-                <Button
-                    size={'small'}
-                    className={s.button}
-                    onClick={onClearFilters}
-                >
-                    clear
                 </Button>
             </div>
         </div>
