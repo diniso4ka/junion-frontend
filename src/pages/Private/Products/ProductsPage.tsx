5f1ef5bf2734e0bcc09f9ea4a66a81b35fcc166d
Evgenii <evgenypetrov5230@gmail.com>
2022-11-28 20:34:27 +0300

    refactor layout search-input, products-page

--- src/pages/Private/Products/ProductsPage.tsx
+++ src/pages/Private/Products/ProductsPage.tsx
@@ -9,7 +9,6 @@ import { searchByIncludes } from 'shared/helpers/filters/search'
 import { clearFilters } from 'app/store/slices/productsFilters/productsFilters'
 import { Modal } from 'components/Modal/Modal'
 import { CreateProductForm } from './CreateProductForm/CreateProductForm'
-import { SideButton } from 'components/SideButton'
 
 const ProductsPage: FC = () => {
     const dispatch = useAppDispatch()
@@ -42,38 +41,40 @@ const ProductsPage: FC = () => {
             className={cls(s.ProductsPage)}
             onClick={() => setFilterIsOpen(false)}
         >
-            <div className={s.navigation}>
-                <h1>Products</h1>
-                <Search
-                    type={'filters'}
-                    value={searchValue}
-                    onChange={e => setSearchValue(e.target.value)}
-                    className={s.search}
-                    toggleOpen={() => setFilterIsOpen(!filterIsOpen)}
-                    onClose={() => setFilterIsOpen(false)}
-                    isOpened={filterIsOpen}
-                    onClick={e => e.stopPropagation()}
-                    canClear={!!searchValue || !!queryString}
-                    onClear={() => onClear()}
-                />
-                <Button
-                    onClick={() => setModalIsOpen(true)}
-                    variant={'rounded'}
-                >
-                    Add new product
-                </Button>
-                <p
-                    className={s.date}
-                >{`${date.mounth} ${date.number}, ${date.year}`}</p>
+            <div className={s.top}>
+                <div className={s.topOne}>
+                    <h1>Products</h1>
+                    <Search
+                        type={'filters'}
+                        value={searchValue}
+                        onChange={e => setSearchValue(e.target.value)}
+                        className={s.search}
+                        toggleOpen={() => setFilterIsOpen(!filterIsOpen)}
+                        onClose={() => setFilterIsOpen(false)}
+                        isOpened={filterIsOpen}
+                        onClick={e => e.stopPropagation()}
+                        canClear={!!searchValue || !!queryString}
+                        onClear={() => onClear()}
+                    />
+                </div>
+                <div className={s.topTwo}>
+                    <Button
+                        onClick={() => setModalIsOpen(true)}
+                        variant={'rounded'}
+                    >
+                        Add new product
+                    </Button>
+
+                    <p className={s.date}>
+                        {`${date.mounth} ${date.number}, ${date.year}`}
+                    </p>
+                </div>
             </div>
             <ProductsTable items={filtredItems} className={s.table} />
             <Modal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)}>
                 <CreateProductForm />
             </Modal>
-            <div className={s.sideButtons}>
-                <SideButton variant='update' />
-                <SideButton variant='delete' />
-            </div>
+            <div className={s.sideButtons}></div>
         </div>
     )
 }
