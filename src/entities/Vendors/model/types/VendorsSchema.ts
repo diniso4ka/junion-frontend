5dad7a222959e6638ec6b730d3906364869a2ccc
deniso4ka <den.zagrosh@gmail.com>
2022-12-01 23:39:45 +0300

    fix

--- src/entities/Vendors/model/types/VendorsSchema.ts
+++ src/entities/Vendors/model/types/VendorsSchema.ts
@@ -1,11 +1,17 @@
 export interface VendorsSchema {
-    items?: []
+    items?: Vendor[]
     quantity: number
     isLoading: boolean
     error?: boolean
 }
 
 export interface Vendor {
+    address: string
+    code: string
+    createdAt: string
+    name: string
+    owner: string
+    regCode: string
+    updatedAt: string
     _id: string
-    quantity: number
 }
