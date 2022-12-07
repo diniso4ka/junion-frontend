fcf6af73bd445997d56854fa08a795402086e8b0
deniso4ka <den.zagrosh@gmail.com>
2022-12-08 00:47:36 +0300

    fix: search

--- src/shared/helpers/filters/search.ts
+++ src/shared/helpers/filters/search.ts
@@ -1,9 +1,10 @@
 export const searchByIncludes = (data, searchValue) => {
     const convertedData = data.map(item => ({
         ...item,
-        searchData: `${item.name} ${item.art} ${item.owner} ${
-            item.vendor
-        } ${item.category.join(' ')}`,
+        searchData: `${item.vendor}-${item.art} ${item.category.join(' ')} ${
+            item.name
+        } ${item.price} ${item.quantity} ${item.unit} ${item.owner}
+        `,
     }))
     const filtredData = convertedData.filter(item =>
         item.searchData.toLowerCase().includes(searchValue.toLowerCase())
